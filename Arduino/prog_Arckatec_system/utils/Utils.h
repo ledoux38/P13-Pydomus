96bf8991a8a177f9294b9b34622e9818e1d01ea2
ledoux <ledoux.florian30@gmail.com>
2018-07-18 09:43:47 +0200

    transfer standard functions to another file

--- Arduino/prog_Arckatec_system/utils/Utils.h
+++ Arduino/prog_Arckatec_system/utils/Utils.h
@@ -1,7 +1,14 @@
 #ifndef Utils_h
 #define Utils_h
+#include <Arduino.h>
 
 void update(int& var, int value, int maxi, int mini);
+
+void update_output_piloted(const int& pin, const bool& state, const int& fixture, const int& value_sensor);
+
+void output_piloted_pwm(const int& param, const int& value, const int& fixture);
+
+void output_piloted(const int& param, const int& value, const int& fixture, const int& value_sensor);
 
 #endif
 
